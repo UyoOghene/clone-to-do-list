229c0f57179c78988077bf2d0ceb02c01e69abb9
Uyooghene <uyooghene01@gmail.com>
2024-03-22 01:41:13 +0100

    march22manos

--- script.js
+++ script.js
@@ -9,71 +9,76 @@ const toDo = document.querySelector('.todo');
 const toDolist = document.querySelector('#todolist');
 
 
-
+console.log((toDolist));
 addtask.addEventListener('click', addTask);
 
 inputPanel.addEventListener('click', hidePanel);
 
 function hidePanel(){
     taskNdate.classList.toggle('hide');
-        if(taskNdate.classList.contains('hide')){
-            inputPanel.innerHTML = 'show input panel';
-        }
-        else{
-            inputPanel.innerHTML = 'hide input panel';
-        }
-    } 
+    if(taskNdate.classList.contains('hide')){
+        inputPanel.innerHTML = 'show input panel';
+    }
+    else{
+        inputPanel.innerHTML = 'hide input panel';
+    }
+} 
+
+function showTask(){
+    toDolist.innerHTML = localStorage.getItem('list');  
+    // let loc = localStorage.getItem('list');
+    console.log(toDolist);
+}
+
  
-function addTask(){
-
-    if(inputTask.value === ''){
-    alert('write somehing');
-    }
-    else {
-        console.log(inputTask.value);
-       let node = document.createElement('div');
-       node.classList.add("tasklist");
-       node.setAttribute('id','tasklist' );
-       let node2 = document.createElement('div');
-       node2.classList.add("txt");
-       node.appendChild(node2);
-       let node3 = document.createElement('h3');
-       node3.setAttribute("id", "nametask");
-       node3.innerHTML = inputTask.value;
-       node2.appendChild(node3);
-       let node4 = document.createElement('p');
-       node4.setAttribute("id", "datetask");
-       node4.innerHTML = inputDate.value;
-       node2.appendChild(node4);
-       let node5 = document.createElement('button');
-       node5.setAttribute("id", "deletebtn" );
-       node5.innerHTML = 'delete';
-       node.appendChild(node5);
-        toDo.appendChild(toDolist); 
-        toDolist.appendChild(node);
-        node5.addEventListener('click', delTask);
-
-    }
-
-    inputTask.value ='';
-    inputDate.value ='';
-    saveData();
-    
-}
-
-function delTask(){
-    console.log('deletefunction');
-    const tlist = document.querySelector('.tasklist');
-    tlist.remove();
-    saveData();
-}
-
 function saveData(){
     localStorage.setItem('list', toDolist.innerHTML);
 }
 
-function showTask(){
-    toDolist.innerHTML = localStorage.getItem('list');  
+function delTask(e){
+    console.log('deletefunction');
+    // const tlist = document.querySelector('.tasklist');
+    // tlist.remove();
+    e.target.parentElement.remove();
+
+    saveData();
+}
+
+
+function addTask(){
+
+    if(inputTask.value === ''){
+        alert('write somehing');
+    }
+    else {
+        console.log(inputTask.value);
+        let node = document.createElement('div');
+        node.classList.add("tasklist");
+        node.setAttribute('id','tasklist' );
+        let node2 = document.createElement('div');
+        node2.classList.add("txt");
+        node.appendChild(node2);
+        let node3 = document.createElement('h3');
+        node3.setAttribute("id", "nametask");
+        node3.innerHTML = inputTask.value;
+        node2.appendChild(node3);
+        let node4 = document.createElement('p');
+        node4.setAttribute("id", "datetask");
+        node4.innerHTML = inputDate.value;
+        node2.appendChild(node4);
+        let node5 = document.createElement('button');
+        node5.setAttribute("id", "deletebtn" );
+        node5.innerHTML = 'delete';
+        node.appendChild(node5);
+        toDo.appendChild(toDolist); 
+        toDolist.appendChild(node);
+        node5.addEventListener('click', delTask);
+
+        inputTask.value ='';
+        inputDate.value ='';
+        saveData();
+
+    }  
 }
 
 showTask();
